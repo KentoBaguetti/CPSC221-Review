6897d3326f2ec8405308a9838b49513bc16478b6
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-04-02 13:19:31 -0700

    basic adjacency matrix graph sstruct

--- Graphs/GraphCode.cpp
+++ Graphs/GraphCode.cpp
@@ -3,7 +3,18 @@
 #include <iostream>
 using namespace std;
 
-struct Vertex {
-    int value;
+struct GraphMatrix {
     
+    vector<vector<int>> AdjacencyMatrix;
+    vector<int> vertices;
+    vector<pair<int,int>> edges;
+
+    void insertVertex (int vertexData);
+    void insertEdge (int edgeData);
+    void removeVertex (int vertex);
+    void removeEdge (int edge);
+
+    vector<pair<int,int>> incidentEdges (int vertex);
+    bool areAdjacent (int vertex1, int vertex2);
+
 };
