f628b634134f205062284d47886013984141209a
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-04-07 18:00:30 -0700

    comments

--- Sorting/Sorting.cpp
+++ Sorting/Sorting.cpp
@@ -58,14 +58,17 @@ void InsertionSort (vector<int>& arr) {
 
 void Merge (vector<int>& arr, int left, int mid, int right) {
 
+    // index variables so we know which part of the subarray we are working with
     int first1 = left;
     int first2 = mid + 1;
     int last1 = mid;
     int last2 = right;
 
+    // temp array
     vector<int> temp;
     int tempIndex = 0;
 
+    // compare elements from each subarray and append the smaller value.
     while (first1 <= last1 && first2 <= last2) {
 
         if (arr[first1] <= arr[first2]) temp.push_back(arr[first1++]);
@@ -73,30 +76,36 @@ void Merge (vector<int>& arr, int left, int mid, int right) {
 
     }
 
+    // if there are elements still remaining in one of the arrays with which we did not add to the temp array, add all the rest to the end of the temp array
     while (first1 <= last1) temp.push_back(arr[first1++]);
     while (first2 <= last2) temp.push_back(arr[first2++]);
 
+    // the temp array is now sorted so place them into the right place in the og array
     for (int i = left; i <= right; i++) {
         arr[i] = temp[tempIndex++];
     }
 
 }
 
+// Recursively breaks the array down into smaller subarrays half the size of its given array
 void MS (vector<int>& arr, int left, int right) {
 
     if (left < right) {
 
         int mid = (left + right) / 2;
 
+        // Recursively break down the array
         MS(arr, 0, mid);
         MS(arr, mid + 1, right);
 
+        // Merge algorithm that sorts the array
         Merge(arr, left, mid, right);
 
     }
 
 }
 
+// the method to used to run merge sort, small signature
 void MergeSort (vector<int>& arr) {
 
     MS(arr, 0, arr.size() - 1);
